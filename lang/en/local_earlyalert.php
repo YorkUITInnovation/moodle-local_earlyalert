21b0da2a10918bdca4447f1b7ae81e9470560591
Patrick Thibaudeau <thibaud@yorku.ca>
2024-11-27 20:59:48 -0500

    Added text for impersonator

--- lang/en/local_earlyalert.php
+++ lang/en/local_earlyalert.php
@@ -54,6 +54,7 @@ $string['exam_instructions'] = '<ul>
 </li>
 </ul>';
 $string['idashboard'] = 'Instructor Dashboard';
+$string['impersonate_user'] = 'Send on behalf of';
 $string['impersonating_user'] = 'Impersonating User';
 $string['generate_reports'] = 'Generate Reports';
 $string['grade'] = 'Grade';
