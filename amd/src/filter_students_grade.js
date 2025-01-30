6de1bba8852da0ed1f0b1c2bcd383e206e2ae097
Christian <christian@vpnyork-1-225.vpn.yorku.ca>
2025-01-30 16:45:10 -0500

    more debug

--- amd/src/filter_students_grade.js
+++ amd/src/filter_students_grade.js
@@ -317,8 +317,10 @@ function check_allnone_listener(selected_students) {
 // needed to watch for changes in the dom to setup the preview buttons
 function setup_preview_emails(templateCache) {
     const observer = new MutationObserver((mutationsList, observer) => {
+        console.log('MutationObserver triggered');
         for (let mutation of mutationsList) {
             if (mutation.type === 'childList') {
+                console.log('Child list mutation detected');
                 const preview_buttons = document.querySelectorAll(".early-alert-preview-button");
                 if (preview_buttons.length > 0) {
                     observer.disconnect(); // Stop observing once the elements are found
