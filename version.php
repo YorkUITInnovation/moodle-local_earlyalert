0ca46d7654e5dbe755e033f72df9915929684ffd
Patrick Thibaudeau <thibaud@yorku.ca>
2025-10-26 18:55:30 -0400

    Updated version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_earlyalert';
 $plugin->release = '1.1.0 (Build 20251025000)';
-$plugin->version = 20251025000;
+$plugin->version = 20251026000;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_BETA;
