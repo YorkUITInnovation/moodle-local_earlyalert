8e5df0051d047503f6565f83bde082215fa4f200
Christian <kep@yorku.ca>
2026-02-03 15:04:15 -0500

    add Early alerts link via hook into moodle nav bar

---
+++ lib.php
@@ -0,0 +1,81 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Library functions for the Early Alert plugin.
+ *
+ * @package     local_earlyalert
+ * @copyright   2024 York University <itinnovation@yorku.ca>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Extends the global navigation with the Early Alert link.
+ *
+ * This function is called when Moodle builds the navigation tree.
+ * It adds a link to the Early Alert tool dashboard in the primary navigation.
+ *
+ * @param global_navigation $navigation The global navigation object
+ */
+function local_earlyalert_extend_navigation(global_navigation $navigation) {
+    global $PAGE;
+
+    // Only add the link if the user has the capability to access Early Alert.
+    if (!has_capability('local/earlyalert:access_early_alert', context_system::instance())) {
+        return;
+    }
+
+    // Create the Early Alert navigation node.
+    $earlyalertnode = $navigation->add(
+        get_string('early_alert', 'local_earlyalert'),
+        new moodle_url('/local/earlyalert/tool_dashboard.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        'earlyalert',
+        new pix_icon('i/navigationitem', '')
+    );
+
+    // Set the node to show in the primary navigation (top menu bar).
+    $earlyalertnode->showinflatnavigation = true;
+}
+
+/**
+ * Add Early Alert link to the primary navigation (navbar).
+ *
+ * This is the Moodle 5.x method for adding items to the primary navigation bar.
+ *
+ * @param navigation_node $parentnode The parent navigation node
+ * @param stdClass $course The course object
+ * @param context $context The current context
+ */
+function local_earlyalert_extend_navigation_frontpage(navigation_node $parentnode, stdClass $course, context $context) {
+    // Only add the link if the user has the capability to access Early Alert.
+    if (!has_capability('local/earlyalert:access_early_alert', context_system::instance())) {
+        return;
+    }
+
+    // Add Early Alert to the navigation.
+    $earlyalertnode = $parentnode->add(
+        get_string('early_alert', 'local_earlyalert'),
+        new moodle_url('/local/earlyalert/tool_dashboard.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        'earlyalert',
+        new pix_icon('i/navigationitem', '')
+    );
+}
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_earlyalert';
-$plugin->release = '2.0.0 (Build 2026012900)';
-$plugin->version = 20260129001;
+$plugin->release = '2.0.1 (Build 2026020300)';
+$plugin->version = 20260203001;
 $plugin->requires = 2024042200; // Moodle 5.1 minimum requirement.
 $plugin->maturity = MATURITY_STABLE;
