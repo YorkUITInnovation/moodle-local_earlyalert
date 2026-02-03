4eef6b5ac9885935cc4401a1135949c2727e1c92
Christian <kep@yorku.ca>
2026-02-03 15:24:10 -0500

    oops readded condition for front page

--- lib.php
+++ lib.php
@@ -64,8 +64,8 @@ function local_earlyalert_extend_navigation(global_navigation $navigation) {
  * @param context $context The current context
  */
 function local_earlyalert_extend_navigation_frontpage(navigation_node $parentnode, stdClass $course, context $context) {
-    // Only add the link if the user has the capability to access Early Alert.
-    if (!has_capability('local/earlyalert:access_early_alert', context_system::instance())) {
+    // Only add the link if the user has the capability to access Early Alert or is a site admin.
+    if (!is_siteadmin() && !has_capability('local/earlyalert:access_early_alert', context_system::instance())) {
         return;
     }
 
